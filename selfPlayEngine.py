6447531b5147dec3bde596db7bb2be90b3007d89
Carl Buchholz <32228189+aGuyLearning@users.noreply.github.com>
2025-05-03 13:02:06 +0200

    Refactor Arena and Coach classes to integrate GameRepresentationFunctional, update game state handling, and improve MCTS implementation for better performance and clarity.

--- selfPlayEngine.py
+++ selfPlayEngine.py
@@ -1,5 +1,6 @@
 import GameRepresentationFunctional 
 from MCTS import MCTS
+from NNet import UltimateTTTNet
 
 def best_move(state):
     mcts = MCTS(state, 10000)
@@ -11,12 +12,12 @@ def best_move(state):
         print("no action found")
 
 if __name__ == "__main__":
-    mcts = MCTS(GameRepresentationFunctional.INITIAL_STATE, 10000)
-    for i in range(90):
+    nnet = UltimateTTTNet()
+    mcts = MCTS(GameRepresentationFunctional.INITIAL_STATE, 25, nnet)
+    for i in range(100):
         mcts.search()
         best_action = mcts.get_best_action()
         if best_action:
-            print(GameRepresentationFunctional.stringRep(*mcts.root.state))
             mcts.update_root(mcts.get_best_action())
         else:
             print(GameRepresentationFunctional.stringRep(*mcts.root.state))
